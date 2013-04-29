ff7b57041aaf4d7be4ae4467adaea6e17e22c4e9
francogpellegrini@gmail.com <francogpellegrini@gmail.com@64ca82ca-4328-1b56-78f9-f9e0f0960e98>
2013-04-29 21:00:27 +0000

    ++ Error descubierto con Randoop ++ Se encontro un error gracias a randoop, el cual debemos reparar porque es importante. Para detectar el mismo, se utilizaron las configuraciones estandard de la herramienta randoop sobre el archivo TreeList.java,  con una cota de tiempo de 5 minutos. Algunos errores se asumen del repOK por lo que se deja su resolucion para el ejercicio 3

---
+++ VyV-TP1/test/randoopFailures/RandoopTest_failure_3_revision_37.java
@@ -0,0 +1,38 @@
+package randoopFailures;
+
+import junit.framework.*;
+
+/**
+ * Este archivo fue generado por randoop (configuracion por defecto, con una
+ * cota de tiempo de 5 minutos), el cual queda como reporte de un importante
+ * error encontrado en la revision 37 del proyecto. Se asume que los errores
+ * provienen del repOK, por lo que se deja su resolucion para el ejercicio 3.
+ * 
+ * @author Pellegrini Franco
+ * 
+ */
+public class RandoopTest_failure_3_revision_37 extends TestCase {
+
+  public static boolean debug = false;
+
+  public void test1() throws Throwable {
+
+    if (debug) System.out.printf("%nRandoopTest_failure_1.test1");
+
+
+    ar.verificacion.validacion.treelistimplementation.TreeList var0 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+    int var1 = var0.size();
+    java.lang.String var2 = var0.toString();
+    var0.add(0, (java.lang.Object)(-1L));
+    var0.add(1, (java.lang.Object)(short)1);
+    int var9 = var0.size();
+    int var10 = var0.size();
+    ar.verificacion.validacion.treelistimplementation.TreeList var12 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+    var0.add(1, (java.lang.Object)var12);
+    
+    // Check representation invariant.
+    assertTrue("Representation invariant failed: Check rep invariant (method repOK) for var0", var0.repOK());
+
+  }
+
+}
---
+++ VyV-TP1/test/randoopFailures/RandoopTest_failure_4_revision_37.java
@@ -0,0 +1,39 @@
+package randoopFailures;
+
+import junit.framework.*;
+
+/**
+ * Este archivo fue generado por randoop (configuracion por defecto, con una
+ * cota de tiempo de 5 minutos), el cual queda como reporte de un importante
+ * error encontrado en la revision 37 del proyecto. Se asume que los errores
+ * provienen del repOK, por lo que se deja su resolucion para el ejercicio 3.
+ * 
+ * @author Pellegrini Franco
+ * 
+ */
+public class RandoopTest_failure_4_revision_37 extends TestCase {
+
+  public static boolean debug = false;
+
+  public void test1() throws Throwable {
+
+    if (debug) System.out.printf("%nRandoopTest_failure_2.test1");
+
+
+    ar.verificacion.validacion.treelistimplementation.TreeList var0 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+    var0.removeAll();
+    var0.removeAll();
+    boolean var3 = var0.isEmpty();
+    var0.add(0, (java.lang.Object)(byte)1);
+    int var7 = var0.size();
+    boolean var8 = var0.isEmpty();
+    int var9 = var0.size();
+    boolean var10 = var0.isEmpty();
+    var0.remove(0);
+    
+    // Check representation invariant.
+    assertTrue("Representation invariant failed: Check rep invariant (method repOK) for var0", var0.repOK());
+
+  }
+
+}
