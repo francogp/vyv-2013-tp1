9ee4164fdff3dccf9a510fc008d7177ceb324e2b
kenviota@gmail.com <kenviota@gmail.com@64ca82ca-4328-1b56-78f9-f9e0f0960e98>
2013-05-06 15:14:43 +0000

    ++ Error descubierto con Randoop ++

--- VyV-TP1/test/randoopFailures/RandoopTest_failure_7_revision_44.java
+++ VyV-TP1/test/randoopFailures/RandoopTest_failure_7_revision_44.java
@@ -1,92 +1,100 @@
 package randoopFailures;
 
-import junit.framework.*;
+import junit.framework.TestCase;
 
+/**
+ * Este archivo fue generado por randoop (configuracion por defecto, con una
+ * cota de tiempo de 5 minutos, exceptuando los metodos toString y finTreeList).
+ * 
+ * @author Pellegrini Franco
+ */
 public class RandoopTest_failure_7_revision_44 extends TestCase {
-
-  public static boolean debug = false;
-
-  public void test1() throws Throwable {
-
-    if (debug) System.out.printf("%nRandoopTest_failure_7_revision44.test1");
-
-    ar.verificacion.validacion.treelistimplementation.TreeList var0 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var1 = var0.size();
-    ar.verificacion.validacion.treelistimplementation.TreeList var3 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var4 = var3.size();
-    boolean var5 = var3.isEmpty();
-    var0.add(0, (java.lang.Object)var3);
-    var3.add(0, (java.lang.Object)(-1.0d));
-    boolean var10 = var3.isEmpty();
-    ar.verificacion.validacion.treelistimplementation.TreeList var12 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    var12.removeAll();
-    boolean var14 = var12.isEmpty();
-    ar.verificacion.validacion.treelistimplementation.TreeList var16 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var17 = var16.size();
-    var16.removeAll();
-    var16.removeAll();
-    var12.add(0, (java.lang.Object)var16);
-    var16.removeAll();
-    var3.add(0, (java.lang.Object)var16);
-    java.lang.Object var24 = var3.get(0);
-    ar.verificacion.validacion.treelistimplementation.TreeList var26 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var27 = var26.size();
-    var26.add(0, (java.lang.Object)100L);
-    int var31 = var26.size();
-    boolean var32 = var26.isEmpty();
-    ar.verificacion.validacion.treelistimplementation.TreeList var34 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    var34.removeAll();
-    boolean var36 = var34.isEmpty();
-    ar.verificacion.validacion.treelistimplementation.TreeList var38 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var39 = var38.size();
-    var38.removeAll();
-    var38.removeAll();
-    var34.add(0, (java.lang.Object)var38);
-    boolean var43 = var34.isEmpty();
-    boolean var44 = var34.isEmpty();
-    var26.add(1, (java.lang.Object)var44);
-    int var46 = var26.size();
-    ar.verificacion.validacion.treelistimplementation.TreeList var48 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    var48.removeAll();
-    boolean var50 = var48.isEmpty();
-    int var51 = var48.size();
-    boolean var52 = var48.isEmpty();
-    int var53 = var48.size();
-    boolean var54 = var48.isEmpty();
-    var26.add(1, (java.lang.Object)var48);
-    var3.add(1, (java.lang.Object)1);
-    boolean var57 = var3.isEmpty();
-    ar.verificacion.validacion.treelistimplementation.TreeList var59 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var60 = var59.size();
-    ar.verificacion.validacion.treelistimplementation.TreeList var62 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var63 = var62.size();
-    boolean var64 = var62.isEmpty();
-    var59.add(0, (java.lang.Object)var62);
-    var62.add(0, (java.lang.Object)(-1.0d));
-    boolean var69 = var62.isEmpty();
-    ar.verificacion.validacion.treelistimplementation.TreeList var71 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    var71.removeAll();
-    boolean var73 = var71.isEmpty();
-    ar.verificacion.validacion.treelistimplementation.TreeList var75 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var76 = var75.size();
-    var75.removeAll();
-    var75.removeAll();
-    var71.add(0, (java.lang.Object)var75);
-    var75.removeAll();
-    var62.add(0, (java.lang.Object)var75);
-    java.lang.Object var83 = var62.get(0);
-    ar.verificacion.validacion.treelistimplementation.TreeList var85 = new ar.verificacion.validacion.treelistimplementation.TreeList();
-    int var86 = var85.size();
-    var85.removeAll();
-    int var88 = var85.size();
-    var85.removeAll();
-    var62.add(2, (java.lang.Object)var85);
-    boolean var91 = var62.isEmpty();
-    var3.add(1, (java.lang.Object)var91);
-    boolean var93 = var3.isEmpty();
-    var3.remove(1);
-    java.lang.Object var97 = var3.get(1);
     
-  }
-
+    public static boolean debug = false;
+    
+    public void test1() throws Throwable {
+        
+        if (debug) {
+            System.out.printf("%nRandoopTest_failure_7_revision44.test1");
+        }
+        
+        ar.verificacion.validacion.treelistimplementation.TreeList var0 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var1 = var0.size();
+        ar.verificacion.validacion.treelistimplementation.TreeList var3 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var4 = var3.size();
+        boolean var5 = var3.isEmpty();
+        var0.add(0, var3);
+        var3.add(0, (-1.0d));
+        boolean var10 = var3.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var12 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        var12.removeAll();
+        boolean var14 = var12.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var16 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var17 = var16.size();
+        var16.removeAll();
+        var16.removeAll();
+        var12.add(0, var16);
+        var16.removeAll();
+        var3.add(0, var16);
+        java.lang.Object var24 = var3.get(0);
+        ar.verificacion.validacion.treelistimplementation.TreeList var26 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var27 = var26.size();
+        var26.add(0, 100L);
+        int var31 = var26.size();
+        boolean var32 = var26.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var34 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        var34.removeAll();
+        boolean var36 = var34.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var38 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var39 = var38.size();
+        var38.removeAll();
+        var38.removeAll();
+        var34.add(0, var38);
+        boolean var43 = var34.isEmpty();
+        boolean var44 = var34.isEmpty();
+        var26.add(1, var44);
+        int var46 = var26.size();
+        ar.verificacion.validacion.treelistimplementation.TreeList var48 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        var48.removeAll();
+        boolean var50 = var48.isEmpty();
+        int var51 = var48.size();
+        boolean var52 = var48.isEmpty();
+        int var53 = var48.size();
+        boolean var54 = var48.isEmpty();
+        var26.add(1, var48);
+        var3.add(1, 1);
+        boolean var57 = var3.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var59 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var60 = var59.size();
+        ar.verificacion.validacion.treelistimplementation.TreeList var62 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var63 = var62.size();
+        boolean var64 = var62.isEmpty();
+        var59.add(0, var62);
+        var62.add(0, (-1.0d));
+        boolean var69 = var62.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var71 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        var71.removeAll();
+        boolean var73 = var71.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var75 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var76 = var75.size();
+        var75.removeAll();
+        var75.removeAll();
+        var71.add(0, var75);
+        var75.removeAll();
+        var62.add(0, var75);
+        java.lang.Object var83 = var62.get(0);
+        ar.verificacion.validacion.treelistimplementation.TreeList var85 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var86 = var85.size();
+        var85.removeAll();
+        int var88 = var85.size();
+        var85.removeAll();
+        var62.add(2, var85);
+        boolean var91 = var62.isEmpty();
+        var3.add(1, var91);
+        boolean var93 = var3.isEmpty();
+        var3.remove(1);
+        java.lang.Object var97 = var3.get(1);
+        
+    }
+    
 }
---
+++ VyV-TP1/test/randoopFailures/RandoopTest_failure_8_revision_47.java
@@ -0,0 +1,87 @@
+package randoopFailures;
+
+import junit.framework.TestCase;
+
+/**
+ * Este archivo fue generado por randoop (configuracion por defecto, con una
+ * cota de tiempo de 5 minutos, exceptuando los metodos toString y finTreeList).
+ * 
+ * @author Sung Pei Jung
+ */
+public class RandoopTest_failure_8_revision_47 extends TestCase {
+    
+    public static boolean debug = false;
+    
+    public void test1() throws Throwable {
+        
+        if (debug) {
+            System.out.printf("%nRandoopTest_failure_8_revision_47.test1");
+        }
+        
+        ar.verificacion.validacion.treelistimplementation.TreeList var0 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var1 = var0.size();
+        ar.verificacion.validacion.treelistimplementation.TreeList var3 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var4 = var3.size();
+        boolean var5 = var3.isEmpty();
+        var0.add(0, var3);
+        var3.add(0, (-1.0d));
+        boolean var10 = var3.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var12 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        var12.removeAll();
+        boolean var14 = var12.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var16 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var17 = var16.size();
+        var16.removeAll();
+        var16.removeAll();
+        var12.add(0, var16);
+        var16.removeAll();
+        var3.add(0, var16);
+        java.lang.Object var24 = var3.get(0);
+        ar.verificacion.validacion.treelistimplementation.TreeList var26 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var27 = var26.size();
+        var26.removeAll();
+        int var29 = var26.size();
+        var26.removeAll();
+        var3.add(2, var26);
+        int var32 = var3.size();
+        var3.remove(0);
+        boolean var35 = var3.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var37 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var38 = var37.size();
+        var37.add(0, 100L);
+        int var42 = var37.size();
+        int var43 = var37.size();
+        java.lang.Object var45 = var37.get(0);
+        var37.remove(0);
+        var37.removeAll();
+        boolean var49 = var37.isEmpty();
+        var3.add(1, var49);
+        boolean var51 = var3.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var53 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var54 = var53.size();
+        ar.verificacion.validacion.treelistimplementation.TreeList var56 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var57 = var56.size();
+        boolean var58 = var56.isEmpty();
+        var53.add(0, var56);
+        var56.add(0, (-1.0d));
+        var56.removeAll();
+        boolean var64 = var56.isEmpty();
+        int var65 = var56.size();
+        boolean var66 = var56.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var68 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        var68.removeAll();
+        boolean var70 = var68.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var72 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var73 = var72.size();
+        var72.removeAll();
+        var72.removeAll();
+        var68.add(0, var72);
+        var56.add(0, var72);
+        var72.removeAll();
+        int var79 = var72.size();
+        var3.add(1, var79);
+        var3.remove(2);
+        
+    }
+    
+}
