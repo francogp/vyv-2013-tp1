047d8bb127c4408317ff851ed781739e53c39f9e
francogpellegrini@gmail.com <francogpellegrini@gmail.com@64ca82ca-4328-1b56-78f9-f9e0f0960e98>
2013-05-06 00:13:38 +0000

    ++ Error descubierto con Randoop ++

---
+++ VyV-TP1/test/randoopFailures/RandoopTest_failure_5_revision_42.java
@@ -0,0 +1,101 @@
+package randoopFailures;
+
+import junit.framework.TestCase;
+
+/**
+ * Este archivo fue generado por randoop (configuracion por defecto, con una
+ * cota de tiempo de 5 minutos).
+ * 
+ * @author Pellegrini Franco
+ * 
+ */
+public class RandoopTest_failure_5_revision_42 extends TestCase {
+    
+    public static boolean debug = false;
+    
+    public void test1() throws Throwable {
+        
+        if (debug) {
+            System.out.printf("%nRandoopTest_failure_2.test1");
+        }
+        
+        ar.verificacion.validacion.treelistimplementation.TreeList var0 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        java.lang.String var1 = var0.toString();
+        java.lang.String var2 = var0.toString();
+        boolean var3 = var0.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var5 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        java.lang.String var6 = var5.toString();
+        boolean var7 = var5.isEmpty();
+        int var8 = var5.size();
+        var5.removeAll();
+        var0.add(0, var5);
+        int var11 = var0.size();
+        java.lang.String var12 = var0.toString();
+        boolean var13 = var0.isEmpty();
+        boolean var14 = var0.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var16 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        java.lang.String var17 = var16.toString();
+        int var18 = var16.size();
+        var16.removeAll();
+        var0.add(0, var16);
+        ar.verificacion.validacion.treelistimplementation.TreeList var22 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        java.lang.String var23 = var22.toString();
+        var22.removeAll();
+        java.lang.String var25 = var22.toString();
+        var22.removeAll();
+        ar.verificacion.validacion.treelistimplementation.TreeList var28 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        java.lang.String var29 = var28.toString();
+        boolean var30 = var28.isEmpty();
+        int var31 = var28.size();
+        java.lang.String var32 = var28.toString();
+        var22.add(0, var32);
+        boolean var34 = var22.isEmpty();
+        var22.remove(0);
+        var0.add(0, 0);
+        ar.verificacion.validacion.treelistimplementation.TreeList var39 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        java.lang.String var40 = var39.toString();
+        var39.removeAll();
+        java.lang.String var42 = var39.toString();
+        var39.removeAll();
+        ar.verificacion.validacion.treelistimplementation.TreeList var45 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        java.lang.String var46 = var45.toString();
+        boolean var47 = var45.isEmpty();
+        int var48 = var45.size();
+        java.lang.String var49 = var45.toString();
+        var39.add(0, var49);
+        var39.remove(0);
+        ar.verificacion.validacion.treelistimplementation.TreeList var54 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        var54.removeAll();
+        var54.removeAll();
+        boolean var57 = var54.isEmpty();
+        java.lang.String var58 = var54.toString();
+        int var59 = var54.size();
+        var39.add(0, var54);
+        boolean var61 = var54.isEmpty();
+        ar.verificacion.validacion.treelistimplementation.TreeList var63 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        var63.removeAll();
+        var63.removeAll();
+        boolean var66 = var63.isEmpty();
+        var54.add(0, var63);
+        var54.remove(0);
+        var54.removeAll();
+        ar.verificacion.validacion.treelistimplementation.TreeList var72 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var73 = var72.size();
+        int var74 = var72.size();
+        java.lang.String var75 = var72.toString();
+        var54.add(0, var72);
+        java.lang.String var77 = var72.toString();
+        boolean var78 = var72.isEmpty();
+        var72.removeAll();
+        var0.add(1, var72);
+        int var81 = var0.size();
+        var0.remove(3);
+        
+        // Check representation invariant.
+        assertTrue(
+                "Representation invariant failed: Check rep invariant (method repOK) for var0",
+                var0.repOK());
+        
+    }
+    
+}
---
+++ VyV-TP1/test/randoopFailures/RandoopTest_failure_6_revision_42.java
@@ -0,0 +1,42 @@
+package randoopFailures;
+
+import junit.framework.TestCase;
+
+/**
+ * Este archivo fue generado por randoop (configuracion por defecto, con una
+ * cota de tiempo de 5 minutos).
+ * 
+ * @author Pellegrini Franco
+ * 
+ */
+public class RandoopTest_failure_6_revision_42 extends TestCase {
+    
+    public static boolean debug = false;
+    
+    public void test1() throws Throwable {
+        
+        if (debug) {
+            System.out.printf("%nRandoopTest_failure_1.test1");
+        }
+        
+        ar.verificacion.validacion.treelistimplementation.TreeList var0 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        int var1 = var0.size();
+        boolean var2 = var0.isEmpty();
+        boolean var3 = var0.isEmpty();
+        boolean var4 = var0.isEmpty();
+        int var5 = var0.size();
+        boolean var6 = var0.isEmpty();
+        var0.add(0, "{}");
+        ar.verificacion.validacion.treelistimplementation.TreeList var11 = new ar.verificacion.validacion.treelistimplementation.TreeList();
+        java.lang.String var12 = var11.toString();
+        var11.add(0, 1);
+        java.lang.String var16 = var11.toString();
+        var11.removeAll();
+        boolean var18 = var11.isEmpty();
+        var0.add(0, var11);
+        boolean var20 = var0.isEmpty();
+        var0.remove(1);
+        
+    }
+    
+}
